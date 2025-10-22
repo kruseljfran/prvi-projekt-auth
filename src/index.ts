bc6971efdb9b0724406e16af7298222bf01920d7
kruseljfran <fk55025@fer.hr>
2025-10-22 20:52:41 +0200

    moving views to public

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, "../public")));
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "../public/views"));
 
 const config = {
   authRequired: false,
